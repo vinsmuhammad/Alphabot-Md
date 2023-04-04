0f89d0bb1d721d1d307dc68cce8b19f366716dab
vinsmuhammad <99631718+vinsmuhammad@users.noreply.github.com>
2023-04-04 14:46:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': '5dLhNR8GOHMK3QN', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
 edit sewabot di file settings.js line 24
